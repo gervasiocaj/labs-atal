3c49b0de942e040413120ac10839a595384c5bee
gervasiocaj <juniorb08@gmail.com>
2014-11-27 20:57:46 +0000

    montando a estrutura do algoritmo

--- lab1prob1.py
+++ lab1prob1.py
@@ -1,5 +1,14 @@
-#init
+# coding: utf-8
 
-
-a = 'hello world'
-print a
+def main():
+    print 'main'
+    
+def backtracking():
+    if True: #todas as casas foram visitadas
+        #imprima o caminho
+        return True
+    else:
+        #Adicione recursivamente o próximo movimento ao vetor
+        #Se o movimento escolhido não satisfaz as condições do problema, remova o movimento do vetor e volte ao passo a)
+        #Se nenhuma das alternativas funcionar, retorne falso
+        return False
